782fda8c8f724a310b27df2b660c87af908f6d61
mayura-alahakoon <mayuraalahakoon@gmail.com>
2022-11-25 13:33:52 +0530

    simple js code for nesting arrays

---
+++ itarate_array.js
@@ -0,0 +1,13 @@
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+
+for (let i = 0 ; i < myArr.length; i++ ){
+  console.log(i);
+  total += myArr[i];
+
+
+}
+console.log(total);
